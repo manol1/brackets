5344196f45661fa425420f4ed8269aa0f10da6ce
manol1 <marsen1491@gmail.com>
2021-08-31 00:10:53 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    const brackets = ['(', ')', '[', ']', '{', '}', '|',];
+    const bracketsPair = {
+      [')']: '(',
+      [']']: '[',
+      ['}']: '{',
+      ['|']: '|',
+    };
+    
+    const open = [];
+    
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      for (let j = 0; j < bracketsConfig[i].length; j++) {
+        if (brackets.includes(bracketsConfig[i][j])) {
+      
+        } else {
+          return false;
+        }
+      }
+    }
+    
+    for (let i = 0; i < bracketsConfig.length; i++) {
+     open[i] = bracketsConfig[i][0];
+    }
+    
+    let stack = [];
+        
+    for (let i = 0; i < str.length; i++) {
+      let currentSymbol = str[i];
+      
+      if (open.includes(currentSymbol)) {
+        stack.push(currentSymbol);
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+    
+        let topElement = stack[stack.length - 1];
+    
+        if (bracketsPair[currentSymbol] === topElement) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+
+    if (stack.length !== 0) {
+        return true;
+    }
+
+    return stack.length === 0;
 }
